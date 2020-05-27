2d4c2e0495dcaaf0fd71acd63061e743903928c5
Loke Gustafsson <lokeg2002@gmail.com>
2020-05-27 17:02:09 +0200

    Add reference simulation parameters

--- src/constants.rs
+++ src/constants.rs
@@ -4,27 +4,33 @@ use crate::vector::Float;
 pub const WIDTH: usize = 800;
 pub const HEIGHT: usize = 800;
 
-// Simulation
+// Computational
 pub const COUNT: usize = 2000;
-pub const RADIUS: Float = 1.0 * LIGHT_YEAR;
-pub const ROTATIONAL_PERIOD: Float = 300_000.0 * YEAR;
-pub const DENSITY_CURVE: DensityCurve = DensityCurve::InverseQuadratic;
-pub const DELTA_T: Float = 50.0 * YEAR;
-
+pub const DELTA_T: Float = 1000.0 * YEAR;
+// General initial conditions
+pub const RADIUS: Float = 10_000.0 * AU;
+pub const ROTATIONAL_PERIOD: Float = 1e9 * YEAR;
+pub const DENSITY_CURVE: DensityCurve = DensityCurve::Uniform;
+// Gravity
 pub const ENABLE_GRAVITY: bool = true;
-pub const MASS: Float = 500.0 * SOLAR_MASS;
-
+pub const MASS: Float = 1.0 * SOLAR_MASS;
+// SPH
 pub const ENABLE_GAS_DYNAMICS: bool = true;
+const INITIAL_TEMPERATURE: Float = 5.0;
+pub const MOLAR_MASS: Float = 0.002016;
 pub const NEIGHBORS: usize = 30;
-pub const SMOOTHING_DIST_FACTOR: Float = 3.0;
-pub const VELOCITY_AVERAGING: Float = 0.1;
-const INITIAL_TEMPERATURE: Float = 10.0;
-pub const MOLAR_MASS: Float = 0.001;
+pub const SMOOTHING_DIST_FACTOR: Float = 2.0;
+pub const VELOCITY_AVERAGING: Float = 1.0;
 
 // Convenience
+#[allow(dead_code)]
 const LIGHT_YEAR: Float = 1e16;
+#[allow(dead_code)]
+const AU: Float = 1.5e11;
+#[allow(dead_code)]
 const SOLAR_MASS: Float = 2e30;
 pub const YEAR: Float = 3e7;
+#[allow(dead_code)]
 pub enum DensityCurve {
     Uniform,
     InverseLinear,
