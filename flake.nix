dbf8e6911b025eb0877c98cc85ad5036c3afb289
Loke Gustafsson <lokegustafsson@pm.me>
2022-10-16 15:28:42 +0200

    use new rust nix template

--- flake.nix
+++ flake.nix
@@ -1,23 +1,49 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.03";
-
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      defaultPackage.${system} = pkgs.mkShell {
-        name = "nixshell";
-        buildInputs = with pkgs; [
-          cargo
-          # cargo build will through minifb -> x11_dl -> pkg-config run:
-          #     pkg-config --variable=libdir x11
-          #     pkg-config --variable=libdir xcursor
-          # This requires the following packages
-          xorg.libX11
-          xorg.libXcursor
-          pkg-config
-        ];
-      };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
+    cargo2nix = {
+      url = "github:cargo2nix/cargo2nix";
+      inputs.rust-overlay.follows = "rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, cargo2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ cargo2nix.overlays.default ];
+          config.allowUnfree = false;
+        };
+        lib = nixpkgs.lib;
+        rust = import ./rust.nix {
+          inherit lib pkgs;
+          workspace-binaries = {
+            binary-accretion = {
+              rpath = p: [ ];
+              run_time_ld_library_path = p: [ p.xorg.libX11 p.xorg.libXcursor ];
+            };
+          };
+          extra-overrides = { mkNativeDep, p }:
+            [ (mkNativeDep "binary-accretion" [ ]) ];
+        };
+      in {
+        devShells.default = rust.rustPkgs.workspaceShell {
+          packages = let p = pkgs;
+          in [
+            p.rust-bin.stable.latest.clippy
+            p.rust-bin.stable.latest.default
+          ] ++ builtins.attrValues rust.packages;
+        };
+
+        packages = rust.packages // { default = rust.packages.binary-accretion; };
+      });
 }
